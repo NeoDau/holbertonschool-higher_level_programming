300e49f26688b8ef353d46e46309943e396273d8
NeoDau <neodau@gmail.com>
2023-01-18 12:04:39 -0800

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def add(num):
-    return f"{num:02d}"
 for num in range(0, 100):
     if num > 0:
-        print(", ", end="")
-#    print(add(num), end="")
-    print("{}".format(add(num)), end="")
-
+        print(f"{num:02d}, ", end="")
+ #   print("{}".format(num), end="")
