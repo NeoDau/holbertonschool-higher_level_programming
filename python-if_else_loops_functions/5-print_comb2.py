e6b4a09935700029e49a29c24fcb706a0aa95a4a
NeoDau <neodau@gmail.com>
2023-01-18 13:49:02 -0800

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    if num > 0:
-        print(f"{num:02d}, ", end="")
+    if num >= 0:
+       print(f"{num:02d}, ", end="")
+    if num == 99:
+        print("{}".fomrat(num))
