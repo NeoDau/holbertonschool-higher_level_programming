e1499bbbddeb08ae5ab9cb21011d9cf904230ee0
NeoDau <neodau@gmail.com>
2023-01-19 05:49:43 -0800

    12-fizzbuzz.py

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,9 +4,9 @@ def fizzbuzz():
         if x % 15 == 0:
             print("FizzBuzz", end=" ")
         elif x % 5 == 0:
-            print("Buzz", end="")
+            print("Buzz", end=" ")
         elif x % 3 == 0:
             print("Fizz", end=" ")
         else:
-            print(x, end=" ")
+            print(x, end="")
         
