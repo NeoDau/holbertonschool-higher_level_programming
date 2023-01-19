70fd8f2a928e4968ee0ebc65bd9b068f7221d7c1
NeoDau <neodau@gmail.com>
2023-01-19 15:41:57 -0800

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,15 +2,15 @@
 if __name__ == "__main__":
     import sys
 
-def argPrint(argv):
-    x = len(argv) - 1
-    if x == 0:
-        print(x, "arguments.")
-    elif x == 1:
-        print(x, "argument:")
-    else:
-        print(x, "arguments:")
-    for a in range(1, x + 1):
-        print("{}: {}".format(a, argv[a]))
+    def argPrint(argv):
+        x = len(argv) - 1
+        if x == 0:
+            print(x, "arguments.")
+        elif x == 1:
+            print(x, "argument:")
+        else:
+            print(x, "arguments:")
+        for a in range(1, x + 1):
+            print("{}: {}".format(a, argv[a]))
 
-argPrint(sys.argv)
+    argPrint(sys.argv)
