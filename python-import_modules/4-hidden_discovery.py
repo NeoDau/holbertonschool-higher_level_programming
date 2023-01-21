f394d7e1fa362a5febc15769034034a6496961d2
NeoDau <neodau@gmail.com>
2023-01-21 14:41:21 -0800

    4-hidden_discovery.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    def hidden():
-        for x in dir(hiden_4):
+    def hiden():
+        for x in dir(hidden_4):
             if x[:2] != "__":
                 print(x)
-    hidden()
+    hiden()
