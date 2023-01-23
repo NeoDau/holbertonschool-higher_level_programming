8986b5dc15350823e24d2f85ead1161ff4eb0de9
NeoDau <neodau@gmail.com>
2023-01-23 14:19:45 -0800

    2-replace_in_list.py

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    for x in range(len(my_list)):
-        my_list[3] = element
-        if idx < 0:
-            return (my_list)
-        else:
-            return (my_list)
+    if idx < 0 or idx >= len(my_list):
+        return (my_list)
+    my_list[3] = element
+    return (my_list)
