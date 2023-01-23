a1ab70eabc4037ffa796f2558c8c6cf3e9cdb70f
NeoDau <neodau@gmail.com>
2023-01-23 08:46:43 -0800

    1-element_at.py

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     for idx in my_list:
         if idx < 0:
             return (None)
-        elif len(my_list) <= idx:
+        if len(my_list) <= idx:
             return (None)
         else:
-            return (my_list[idx]) 
+            return (my_list[idx])
