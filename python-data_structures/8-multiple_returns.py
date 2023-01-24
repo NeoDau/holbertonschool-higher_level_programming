89ec67b05aa504ad5280fa6884f1440a08d508fe
NeoDau <neodau@gmail.com>
2023-01-24 07:17:51 -0800

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     a = len(sentence)
-    b = sentence[0]
     if a == 0:
         return (0, "none")
+    else:
+        b = sentence[0]
+
     return (a, b)
