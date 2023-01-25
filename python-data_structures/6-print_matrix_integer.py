d9d6efaa0d3c574588c66da47d311e27866cd6b8
NeoDau <neodau@gmail.com>
2023-01-25 07:04:54 -0800

    6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix) == 0:
-        print("{:d}".format(matrix), end="")
     for x in matrix:
+        if len(matrix) > 0:
+            print(end="")
         for a in range(len(x)):
             if a == len(x) - 1:
                 print("{:d}".format(x[a]))
