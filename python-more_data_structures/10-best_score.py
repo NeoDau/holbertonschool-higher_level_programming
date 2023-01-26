c3a6ae29b806e4c5da39b9ab2c5171886cfc507c
NeoDau <neodau@gmail.com>
2023-01-26 11:24:51 -0800

    10-best_score.py

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if len(a_dictionary) is 0:
         return None
     bigValue = max(a_dictionary, key=lambda x: a_dictionary[x])
     return bigValue
