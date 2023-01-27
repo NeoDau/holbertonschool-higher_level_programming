a644a6da2bcf83edcfbfadfeaeaf167555b1ef71
NeoDau <neodau@gmail.com>
2023-01-27 07:43:21 -0800

    2-safe_print_list_integers.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     pi = 0
-    try:
-        for n in range(x):
+    for n in range(x):
+        try:
             print("{:d}".format(my_list[n]), end="")
             pi += 1
-    except (ValueError, TypeError):
-        pass
+        except (ValueError, TypeError):
+            pass
     print()
-    return pi
-
-            
+    return pi 
