7eee43119716629d5bb1e4d16e33ccf97ded350b
NeoDau <neodau@gmail.com>
2023-01-26 19:55:06 -0800

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     a = 0
     for a in range(x):
         try:
-            print(my_list[a],end="")
+            print(my_list[a], end="")
         except IndexError:
             print()
             return a
