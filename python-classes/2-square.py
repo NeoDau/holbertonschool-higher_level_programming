f65ee9d37bb5798cc740574dca58c46c8eaa1206
NeoDau <neodau@gmail.com>
2023-01-31 08:00:41 -0800

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ Write a class Square that defines a square """
+
+
 class Square:
     """ Declarandin square whit size """
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """ initialize squere """
         if type(size) == int:
-            if size >= 0:
-                self.__size = size
-            else:
-                raise TypeError ("size must be an integer")
+            self.__size = size
         else:
-            raise ValueError ("size must be >= 0")
-             
-            
+            raise TypeError("size must be an integer")
+        if size >= 0:
+            self.__size = size
+        else:
+            raise ValueError("size must be >= 0")
