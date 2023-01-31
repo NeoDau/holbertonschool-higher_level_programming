b05a04cc9828d061d0a0e1d20016142970b7fa29
NeoDau <neodau@gmail.com>
2023-01-31 08:44:05 -0800

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+""" Write a class Square that defines a square """
+
+
 class Square:
+    """ Initialize square """
     def __init__(self, size=0):
         self.__size = size
-
-    def area (self):
+    """ Initialize area """
+    def area(self):
         area = self.__size * self.__size
         return (area)
