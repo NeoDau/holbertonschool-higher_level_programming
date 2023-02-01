482274bbe5aca176d535e657b7153a6e4d5d2d59
NeoDau <neodau@gmail.com>
2023-02-01 07:16:07 -0800

    6-square.py

---
+++ python-classes/6-square.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """Initialize Squere"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize class"""
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """return area"""
+        return self.__size
+
+    @property
+    def position(self):
+        """Return position Square"""
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if type(value) == int:
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+        if value >= 0:
+            self.__size = value
+        else:
+            raise ValueError("size must be >= 0")
+
+    @position.setter
+    def position(self, value):
+        if type(value[0]) == int or type(value[1]) == int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self__position = value
+
+    def area(self):
+        """returns areas"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print square"""
+
+        if self.__size == 0:
+            print()
+        else:
+            for x in range(self.__position[1]):
+                print("")
+            for a in range(self.__size):
+                for space in range(self.__position[0]):
+                    print(" ", end="")
+                for i in range(self.__size):
+                    print('#', end="")
+                if a+1 < self.__size:
+                    print("")
+            print("")
