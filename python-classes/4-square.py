720cf2b161e381cedc0099ad0d0636118a9b2b59
NeoDau <neodau@gmail.com>
2023-01-31 16:20:08 -0800

    4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """Initialize Squere"""
+
+    def __init__(self, size=0):
+        """Initialize class"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """return area"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) == int:
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+        if value >= 0:
+            self.__size = value
+        else:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """returns areas"""
+        return self.__size ** 2
