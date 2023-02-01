f4e94676bfefcc896c8f09135adaafdeae70d6bf
NeoDau <neodau@gmail.com>
2023-02-01 10:16:58 -0800

    0-add_integer.py & test 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -2,7 +2,7 @@
 """shebang"""
 
 
-def add_integer(a,b=98):
+def add_integer(a, b=98):
     """add integer"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
