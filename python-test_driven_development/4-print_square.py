67ea20ab06579b2ed84f86f998d45e17a5734561
NeoDau <neodau@gmail.com>
2023-02-01 17:20:39 -0800

    4-print_square.py

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""shebang"""
+
+
 def print_square(size):
+    """print Square"""
     if not isinstance(size, int) or isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     if size < 0:
@@ -8,5 +12,3 @@ def print_square(size):
         for a in range(size):
             print("#", end="")
         print("")
-
-
