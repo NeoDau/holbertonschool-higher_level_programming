8ac66e17515e834da8dc2e7883e93cf3077f1aad
NeoDau <neodau@gmail.com>
2023-02-07 08:30:24 -0800

    test

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+def is_kind_of_class(obj, a_class):
+    """initializanding function"""
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
