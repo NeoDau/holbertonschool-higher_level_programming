014ef114112ecb2840147e00b708a0a37ddb54db
NeoDau <neodau@gmail.com>
2023-02-07 08:42:53 -0800

    3-is_kind_of_class.py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,7 @@
 
 def is_kind_of_class(obj, a_class):
     """initializanding function"""
-    if type(obj) is a_class:
+    if isinstance(obj, a_class):
         return True
     else:
         return False
