b12386f845223175fe824481e24122ef682210af
NeoDau <neodau@gmail.com>
2023-02-08 08:42:14 -0800

    8-rectangle.py

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,11 +2,12 @@
 """shebang"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-class Rectangle(BaseException):
+
+class Rectangle(BaseGeometry):
     """Initializanding class"""
     def __init__(self, width, height):
         """Function"""
         self.__width = width
         self.__height = height
-        super().__init__(width)
-        super().__init__(height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
