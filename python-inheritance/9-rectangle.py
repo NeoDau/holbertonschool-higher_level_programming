d9e22d3f3efa6b671b255fbca270818397fde69c
NeoDau <neodau@gmail.com>
2023-02-08 10:02:13 -0800

    9-rectangle.py

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""shebang"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Initializanding class"""
+    def __init__(self, width, height):
+        """Function"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """Area of rectangle"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """STR"""
+        return (f"[Rectangle] {self.__width}/{self.__height}")
