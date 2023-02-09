4cf7f1d2926b16fc4b15ee2a1704085729eac46f
NeoDau <neodau@gmail.com>
2023-02-09 05:05:46 -0800

    7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -4,7 +4,7 @@
 
 class BaseGeometry:
     """Class BaseGeometry"""
-    
+
     def area(self):
         """initializandig"""
         raise Exception("area() is not implemented")
@@ -15,3 +15,4 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
