798743c07b4db0e314cb672505d59aa41df46abc
NeoDau <neodau@gmail.com>
2023-02-10 12:21:43 -0800

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as f:
-        f = read_file
-        print(f)
+    with open(filename, encoding="utf-8") as f:
+        x = f.read()
+        print(x, end="")
