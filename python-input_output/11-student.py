d11febebc7f380c1d973a5f0f00785b108c5041c
NeoDau <neodau@gmail.com>
2023-02-11 11:11:30 -0800

    11-students.py

---
+++ python-input_output/11-student.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+class Student:
+    """create class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            newDict = {}
+            for x in attrs:
+                if x in self.__dict__.keys():
+                    newDict[x] = self.__dict__[x]
+            return newDict
+
+    def reload_from_json(self, json):
+        for value, key in json.items():
+            setattr(self, key, value)
