acab45c4752da8e24e54fdf1a800cd39310415cf
NeoDau <neodau@gmail.com>
2023-02-15 06:03:15 -0800

    rectangle.py

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""shebang"""
+"""shebang."""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """class"""
+    """class."""
     def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
