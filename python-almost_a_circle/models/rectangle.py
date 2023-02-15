1375a0fb0da6d50b3380593a2a20a85f925cab83
NeoDau <neodau@gmail.com>
2023-02-15 05:25:58 -0800

    rectangle

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+class Rectangle(Base):
+    """class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+        super().__init__(id)
