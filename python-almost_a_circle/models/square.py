2ab0d581a3987a088e2dd11a5c7d8e94df13b205
NeoDau <neodau@gmail.com>
2023-02-15 17:39:44 -0800

    square check

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """class saquare"""
     def __init__(self, size, x=0, y=0, id=None):
         """init new instance"""
-        super().__init__(size, size, id, x, y)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """str init"""
