936e1c652edc75f1aac0c75fe65dbd80ac90c524
NeoDau <neodau@gmail.com>
2023-02-15 16:17:37 -0800

    square.py

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """init new instance"""
         super().__init__(size, size, id, x, y)
-  
+
     def __str__(self):
         """str init"""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
@@ -17,12 +17,7 @@ class Square(Rectangle):
     def size(self):
         return self.width
 
-
     @size.setter
     def size(self, value):
         self.height = value
         self.width = value
-
-    def area(self):
-        """function area"""
-        return self.size * self.size
