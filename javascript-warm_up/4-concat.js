d6ca3b775624977feb5a0ccc9e8e6d359372b650
NeoDau <neodau@gmail.com>
2023-04-14 09:14:19 -0700

    concatenando variables

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-
-const [primer_palabra] = process.argv.slice(2)
-const [segunda_palabra] = process.argv.slice(3)
-
+const [primer_palabra] = process.argv.slice(2);
+const [segunda_palabra] = process.argv.slice(3);
 
 if (primer_palabra + segunda_palabra) {
-	console.log(primer_palabra + " is " + segunda_palabra)
-} else{
-	console.log("undefined is undefined")
+  console.log(primer_palabra + ' is ' + segunda_palabra);
+} else {
+  console.log('undefined is undefined');
 }
