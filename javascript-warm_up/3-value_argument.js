7092d07db36495c2d3a4ec367924fa41ed987e17
NeoDau <neodau@gmail.com>
2023-04-14 08:46:36 -0700

    value the argument

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
+const [args] = process.argv.slice(2);
 
-if (args === 0) {
-  console.log('No argument');
-} else if (args === 1) {
-  console.log('Argument found');
+if (args) {
+  console.log(args);
 } else {
-  console.log('Arguments found');
+  console.log("No argument");
 }
