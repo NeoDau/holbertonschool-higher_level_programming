69fee8047f0fb636d8f982e4695118c20ca4717f
NeoDau <neodau@gmail.com>
2023-04-15 15:08:57 -0700

    loop C is fun

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const num = parseInt(args);
+let x;
+
+if (isNaN(num)){
+	console.log("Missing number of occurrences");
+} else {
+	for(x = 0; x < num; x++);
+	{
+		console.log("C is fun");
+	}
+}
