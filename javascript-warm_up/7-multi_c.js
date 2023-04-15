0148ae85ebc955f5baf05b458b8f9dc17ae60749
NeoDau <neodau@gmail.com>
2023-04-15 15:14:52 -0700

    loop but C is fun

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,11 +4,10 @@ const args = process.argv.slice(2);
 const num = parseInt(args);
 let x;
 
-if (isNaN(num)){
-	console.log("Missing number of occurrences");
+if (isNaN(num)) {
+  console.log('Missing number of occurrences');
 } else {
-	for(x = 0; x < num; x++);
-	{
-		console.log("C is fun");
-	}
+  for (x = 0; x < num; x++) {
+    console.log('C is fun');
+  }
 }
