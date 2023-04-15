c2e883e7dda978b7bbcdcebb28c568da645cfd30
NeoDau <neodau@gmail.com>
2023-04-15 14:48:01 -0700

    6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const lista = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const lista = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let x;
 
-for (x = 0; x < lista.length; x++){
-	console.log(lista[x])
+for (x = 0; x < lista.length; x++) {
+  console.log(lista[x]);
 }
