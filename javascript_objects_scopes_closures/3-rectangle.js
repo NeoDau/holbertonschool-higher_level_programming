56ba4d25a222ca8badf7ea664dcbeac470da2481
NeoDau <neodau@gmail.com>
2023-04-16 13:45:07 -0700

    print function repeat

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -3,18 +3,19 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0) {
-     	return;
+	return;
     } else {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
-  	if (this.width && this.height) {
-		for (let i = 0; i < this.height; i++) {
-      			console.log("X".repeat(this.width));
-    			}
-  		}
-	}
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
 }
 module.exports = (Rectangle);
