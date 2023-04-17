c6691ab97daf21312c6366c063000a7a537ad7f6
NeoDau <neodau@gmail.com>
2023-04-17 06:30:42 -0700

    write str to file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,12 +2,11 @@
 
 const fs = require('fs');
 
-const filePath = './myFile.txt';
+const filePath = './my_file.txt';
 const content = 'Python is cool';
 
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
     console.error(err);
-    return;
   }
 });
