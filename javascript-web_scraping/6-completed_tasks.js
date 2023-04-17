364b20e10b4b6b0a8b5f02dece917e559bc52710
NeoDau <neodau@gmail.com>
2023-04-17 07:03:47 -0700

    number of tasks completed by usr id

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,31 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-const url = process.argv[2];
-
-request(url, (error, response, body) => {
+request(apiUrl, (error, response, body) => {
   if (error) {
     console.error(error);
     return;
   }
-
-  if (response.statusCode !== 200) {
-    console.error(`Unexpected response: ${response.statusCode}`);
-    return;
-  }
-
+  
   const todos = JSON.parse(body);
-  const completedByUser = {};
-
-  todos.forEach(todo => {
+  const completedUser = {};
+  
+  todos.forEach((todo) => {
     if (todo.completed) {
-      const userId = todo.userId;
-      completedByUser[userId] = (completedByUser[userId] || 0) + 1;
+      if (completedUser[todo.userId]) {
+        completedUser[todo.userId]++;
+      } else {
+        completedUser[todo.userId] = 1;
+      }
     }
   });
-
-  Object.entries(completedByUser).forEach(([userId, count]) => {
-    console.log(`'${userId}': ${count},`);
-  });
+  console.log(completedUser);
 });
