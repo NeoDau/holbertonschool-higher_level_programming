af7e634316aa2806388e7a48a6b1476c0e324a59
NeoDau <neodau@gmail.com>
2023-04-19 10:07:15 -0700

    webpage stores in a file

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -7,8 +7,8 @@ const URL = process.argv[2];
 const FILE = process.argv[3];
 
 function requestStore (err, resp, body) {
-	if (err == null) {
-		fs.writeFileSync(FILE, body);
-	}
+  if (err == null) {
+    fs.writeFileSync(FILE, body);
+  }
 }
 request(URL, requestStore);
